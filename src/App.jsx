fc09376daaa2716cceb75cbe28879188a804b218
Leandro Paisark <lletorres1988@gmail.com>
2024-03-06 00:28:59 -0300

    LeandroPaisarkReto05

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ComponenteConteiner from './ComponenteConteiner'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Hola</h1>
+    <ComponenteConteiner></ComponenteConteiner>
+    </>
+  )
+}
+
+export default App
---
+++ src/ComponenteConteiner.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function ComponenteConteiner() {
+
+
+  return (
+    <>
+    <h1>mi componente Conteiner</h1>
+    </>
+  )
+}
+
+export default ComponenteConteiner
